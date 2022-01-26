2e191df4fb94f97b03876881ac199da858c71201
Alexander Reitzel <funtimecoding@gmail.com>
2022-01-26 15:38:20 +0100

    add global gitignore

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 cd "${SCRIPT_DIRECTORY}"
-FILES=$(find . -maxdepth 1 -name '.*' -not -path './.git' -and -not -path . | grep --extended-regexp --invert-match "(\.idea|\.gitignore)" | awk -F / '{ print $NF }')
+FILES=$(find . -maxdepth 1 -name '.*' -not -path './.git' -and -not -path . | grep --extended-regexp --invert-match "(\.idea|\.gitignore)$" | awk -F / '{ print $NF }')
 
 for FILE in ${FILES}; do
     ln -snf "${PWD}/${FILE}" "${HOME}/${FILE}"
