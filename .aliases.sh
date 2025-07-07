24b70d4e7bf79c04faced4ec1755b7649856affa
Alexander Reitzel <funtimecoding@gmail.com>
2025-07-07 16:25:03 +0200

    alias

--- .aliases.sh
+++ .aliases.sh
@@ -30,6 +30,7 @@ alias gbd='git branch --delete'
 alias gba='git branch --all'
 alias gbv='git branch -vv'
 alias gsl='git stash list --date local'
+alias ggs='gogitstatus'
 
 # Docker
 alias d='docker'
